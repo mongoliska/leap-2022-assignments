c1b54c888386867d60e08ef26e2961b40016b235
Your Name <you@example.com>
2022-12-02 16:31:31 +0800

    added

---
+++ javascript/JS Basic/datatypes.js
@@ -0,0 +1,6 @@
+let har = 5; 
+let tsagaan = 'string';
+let narsun = true;
+let todorhoigui;
+let umd = null; 
+console.log (typeof(too));
---
+++ javascript/JS Basic/variable.js
@@ -0,0 +1,5 @@
+let math = 9; 
+let bichver = 'string';
+let unen = true;
+let undef;
+let hooson = null; 

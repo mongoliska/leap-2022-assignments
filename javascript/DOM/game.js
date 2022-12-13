20c440613b161b3497ed9495e79b2c32e8daa492
Your Name <you@example.com>
2022-12-13 16:54:34 +0800

    added

--- javascript/DOM/game.js
+++ javascript/DOM/game.js
@@ -1,13 +1,74 @@
-let score = 0;
+let SCORE = 0;
+const TITLE_COUNT = 9; 
+const TIMER = 10;
+let MILLISECONDS = 0;
+let SECONDS = TIMER;
+const isGameOver = false;
+
+
 const scoreTarget = document.querySelector("#scoreTarget");
+const gameOverScoreTarget = document.querySelector("#gameOverTarget")
+const parent=document.querySelector('ul');  
+const timerTarget= document.querySelector("h5");
+const gameOverScreen = document.querySelectorAll(".game-over-screen") [0];
+
+function gameOver(){
+    console.log(gameOverScreen.classList.add("active")); //.remove .add .toggle (baival hasna, baihguig nemne)
+    isGameOver = true;
+}
+
+function updateTimer(){
+    if(MILLISECONDS <= 0){
+        SECONDS--;
+        MILLISECONDS = 990;
+    }
+    MILLISECONDS -= 10;
+
+    if (MILLISECONDS <= 0){
+        SECONDS--;
+        MILLISECONDS = 999;
+    }
+    MILLISECONDS -= 1;
+
+    if(MILLISECONDS <= 0 && SECONDS <= 0){
+        clearInterval(countDown);
+        gameOver();
+    }
+
+    // function gameOver (){
+    //     alert("your game is iver you scored:" +SCORE);
+    //     isGameOver = true;
+    // }
+        
+    
+    let timerResult="";
+    if(SECONDS<10){
+        timerResult += "0";
+    }
+    timerResult +=SECONDS
+    timerResult += ":";
+    if (MILLISECONDS < 100){
+        timerResult + "0";
+    }
+    if (MILLISECONDS<10){
+        timerResult + "0";
+    }
+    timerResult += MILLISECONDS;
+    timerTarget.innerHTML = timerResult;
+}
+    const countDown = setInterval(updateTimer, 10);
 
 function updateScore(point){
     score+=point;
     scoreTarget.innerHTML = score;
 }
 
+function updateScore (point){
+    SCORE += point;
+    scoreTarget.innerHTML = SCORE;
+    gameOverScoreTarget.innerHTML = SCORE;
+}
 
-const parent=document.querySelector('ul');  
 function getRandomNumber(min , max){     
     return Math.floor(Math.random()*(max-min))+min;  
 } 
@@ -16,9 +77,7 @@ function getRandomColor() {
     const red = getRandomNumber(0, 255);     
     const blue = getRandomNumber(0, 255);     
     return [red,green,blue];  
-}
-
-const titleCount = 9;                  
+}             
 
 function getDifferentColor(colors){
     const random = getRandomNumber(0,2);
@@ -40,7 +99,7 @@ function reDraw() {
 
 const randomIndex = getRandomNumber(0,8);
 
-for(let i = 0; i < titleCount; i++){
+for(let i = 0; i < TITLE_COUNT; i++){
     const li = document.createElement('li');
     const isNormal = i !==randomIndex;
     if (isNormal){

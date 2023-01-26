e76d6461eb92b08673183b5a757fec278866c78f
Your Name <you@example.com>
2023-01-26 17:39:29 +0800

    added

--- Express/intro/app.js
+++ Express/intro/app.js
@@ -5,7 +5,10 @@ const app = express();
 app.use(cors())
 const port = 8000;
 
-const categories = [
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json();
+
+let categories = [
     {
         id: 1,
         name: 'Menu',
@@ -19,6 +22,8 @@ const categories = [
         name: 'Combo',
     },
 ]
+
+let nextCatId = categories.length
 
 const articles = [
     {
@@ -78,13 +83,64 @@ app.get('/categories', (request, response) => {
     response.json(categories);
 });
 
+// app.get('/categories/:id', (request, response) => {
+//     const { id } = request.params;
+
+//     const filteredArr = articles.filter(e => {
+//         return e.categoryId === Number(id) });
+//     console.log(filteredArr)
+//     response.json(filteredArr);
+// });
+
 app.get('/categories/:id', (request, response) => {
     const { id } = request.params;
+    let category = null;
 
-    const filteredArr = articles.filter(e => {
-        return e.categoryId === Number(id) });
-    console.log(filteredArr)
-    response.json(filteredArr);
+    for (const cat of categories) {
+        if (id == cat.id) {
+            category = cat;
+            break;
+        }
+    }
+    response.json(category);
+});
+
+app.delete('/categories/:id', (req, res) => {
+    const {id} = req.params;
+    categories = categories.filter((row) => row.id !==id);
+    res.json(id);
+});
+
+app.post('/categories', jsonParser, (req, res) => {
+    const { name } = req.body;
+    const newCategory = {id: nextCatId++, name};
+    // categories.push(newCategory);
+    res.send(newCategory)
+});
+
+app.put('/categories/:id', (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    
+    let updatedCat;
+    categories = categories.map((row) => {
+        console.log('aaa');
+        if(row.id === Number(id)) {
+            updatedCat = {id: Number(id), name};
+            return updatedCatl
+        }
+        return row;
+    });
+    res.json(updatedCat);
+    // const index = categories.findIndex((item) => item.id === Number(id));
+    // if (index === -2) {
+    //     res.status(400).json('Bad request');
+    // } else {
+    //     const updatedCategory = categories[index];
+    //     updatedCategory.name = name;
+    //     categories[index] = updatedCategory;
+    //     res.json(updatedCategory);
+    // }
 });
 
 app.get('/articles', (request, response) => {
